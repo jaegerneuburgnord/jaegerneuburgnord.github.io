ba3fda0be7bc4c850cdf479510a024710a8aaf05
jaegerneuburgnord <jaegerneuburgnord@gmail.com>
2025-03-20 23:23:55 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+
+const cacheName = 'wiesen-karte-cache';
+const filesToCache = [
+  './',
+  './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js',
+  'https://cdn.jsdelivr.net/npm/@tmcw/togeojson@4.5.0/dist/togeojson.umd.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
