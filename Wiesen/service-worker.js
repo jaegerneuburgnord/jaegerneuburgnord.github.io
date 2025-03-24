3bf7bf76727cc1e2e802577a23d837a80390dd88
jaegerneuburgnord <jaegerneuburgnord@gmail.com>
2025-03-24 18:50:56 +0100

    Update service-worker.js

--- Wiesen/service-worker.js
+++ Wiesen/service-worker.js
@@ -1,5 +1,5 @@
 // Ändere die Cache-Version, wenn du Updates bereitstellen möchtest
-const cacheName = 'wiesen-karte-cache-v5';
+const cacheName = 'wiesen-karte-cache-v6';
 const filesToCache = [
   './',
   './index.html',
