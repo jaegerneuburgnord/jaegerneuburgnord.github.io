0691eba3677471173bf9b14e3c211f332f196d19
Wolfgang Meyerle <wolfgang.meyerle@mailbox.org>
2025-03-30 15:14:39 +0200

    Neu

--- Wiesen/js/segment-anything.js
+++ Wiesen/js/segment-anything.js
@@ -263,7 +263,7 @@ async function performBrowserSegmentation(lat, lng, bounds, zoom, config) {
                    : config.modelLoadPathEncoder + '/mobile_sam.onnx')
                 : './models/segment-anything-lite/mobile_sam.onnx';
             
-            console.log("ModellpfadEncoder:", modelPath);
+            console.log("ModellpfadEncoder:", modelLoadPathEncoder);
             
             // Prüfen, ob ONNX verfügbar ist
             if (window.ort) {
